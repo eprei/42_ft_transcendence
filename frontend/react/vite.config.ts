2d5ced3e3c9ca9dbb8faf5961de0cf997798e2fa
kayz <robinburri@tutanota.com>
2023-05-06 18:24:18 +0200

    added google rule to eslintrc and formated all files

--- frontend/react/vite.config.ts
+++ frontend/react/vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    strictPort: true,
-    port: 5173
-  }
+    plugins: [react()],
+    server: {
+        host: true,
+        strictPort: true,
+        port: 5173,
+    },
 })
