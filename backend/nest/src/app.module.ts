4c31f96f5c647aefb862be4e8b677feaad0c8838
kayz <robinburri@tutanota.com>
2023-05-08 21:37:36 +0200

    db connection fixed

--- backend/nest/src/app.module.ts
+++ backend/nest/src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
-        // TypeOrmModule.forRoot({
-        //   type: 'postgres',
-        //   url: process.env.DATABASE_URL,
-        //   autoLoadEntities: true,
-        //   synchronize: true,
-        // }),
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          url: process.env.DATABASE_URL,
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
     ],
     controllers: [AppController],
     providers: [AppService],
