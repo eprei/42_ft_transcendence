a1e45980eafc5f5d7c7faa228a585a7e147de8c1
|°_°| <grivel.theo@protonmail.com>
2023-05-08 23:52:54 +0200

    script: add db name into the url

--- create-env.sh
+++ create-env.sh
@@ -11,19 +11,20 @@ alias pwgen="docker run \
 generate_postgres_credentials () {
 	POSTGRES_USER=$(whoami)
 	POSTGRES_PASSWORD=$(pwgen)
+	POSTGRES_NAME="our-data"
 }
 
 create_env_postgres () {
 	cat > env/postgres.env <<- eof
 	POSTGRES_USER=${POSTGRES_USER}
 	POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
-	POSTGRES_DB="bigdata-transcendence"
+	POSTGRES_DB=${POSTGRES_NAME}
 	eof
 }
 
 create_env_nest () {
 	cat > env/nest.env <<- eof
-	DATABASE_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/bigdata-transcendence
+	DATABASE_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/${POSTGRES_NAME}
 	PORT=3000
 	eof
 }
