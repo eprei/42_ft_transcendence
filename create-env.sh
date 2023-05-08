7317f7b29c1514376fa50b997444383f09c5ec61
|°_°| <grivel.theo@protonmail.com>
2023-05-08 23:48:30 +0200

    script: rework tabulation

--- create-env.sh
+++ create-env.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 alias pwgen="docker run \
-    --rm \
-    --interactive \
-    backplane/pwgen \
+	--rm \
+	--interactive \
+	backplane/pwgen \
 	--ambiguous \
 	--capitalize \
-	--secure 20 1
-	" 
+	--secure 20 1"
 
 generate_postgres_credentials () {
 	POSTGRES_USER=$(whoami)
@@ -15,18 +14,18 @@ generate_postgres_credentials () {
 }
 
 create_env_postgres () {
-	cat > env/postgres.env << eof
-POSTGRES_USER=${POSTGRES_USER}
-POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
-POSTGRES_DB="bigdata-transcendence"
-eof
+	cat > env/postgres.env <<- eof
+	POSTGRES_USER=${POSTGRES_USER}
+	POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+	POSTGRES_DB="bigdata-transcendence"
+	eof
 }
 
 create_env_nest () {
-	cat > env/nest.env << eof
-DATABASE_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/db
-PORT=3000
-eof
+	cat > env/nest.env <<- eof
+	DATABASE_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/db
+	PORT=3000
+	eof
 }
 
 main () {
