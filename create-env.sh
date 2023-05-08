324c7265be5f4ce19377532a52b3eaa29b5a3354
Theo Grivel <grivel.theo@protonmail.com>
2023-05-08 16:05:46 +0200

    script: give the db credentials to nest.env

--- create-env.sh
+++ create-env.sh
@@ -10,23 +10,29 @@ alias pwgen="docker run \
 	--secure 20 1
 	" 
 
+generate_postgres_credentials () {
+	POSTGRES_USER=$(whoami)
+	POSTGRES_PASSWORD=$(pwgen)
+}
+
 create_env_postgres () {
 	cat > env/postgres.env << eof
-POSTGRES_USER=$(whoami)
-POSTGRES_PASSWORD=$(pwgen)
+POSTGRES_USER=${POSTGRES_USER}
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
 POSTGRES_DB="bigdata-transcendence"
 eof
 }
 
 create_env_nest () {
 	cat > env/nest.env << eof
-DATABASE_URL=postgres://user::password@postgres:5432/db
+DATABASE_URL=postgres://${POSTGRES_USER}::${POSTGRES_PASSWORD}@postgres:5432/db
 PORT=3000
 eof
 }
 
 main () {
 	mkdir -p env
+	generate_postgres_credentials
 	create_env_nest
 	create_env_postgres
 }
