915906acde362949825aae64872cff39b479b2d4
Robin Burri <robinburri@tutanota.com>
2023-05-17 14:45:15 +0200

    delete two controller

--- backend/src/ninjas/ninjas.controller.ts
+++ backend/src/ninjas/ninjas.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+
+	@Get()
+	getNinjas() {
+		return [];
+	}
+
+	@Get(':id')
+	getOneNinjas() {
+		return {};
+	}
+
+	@Post()
+	createNinjas() {
+		return {};
+	}
+	
+}
