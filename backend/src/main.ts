dc278296c3412048dc2001f44d5d46637b7772eb
Robin Burri <robinburri@tutanota.com>
2023-05-22 15:03:30 +0200

    added all package from scratch

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+
+    const corsOptions: CorsOptions = {
+        origin: 'http://localhost:4040',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type, Accept',
+    }
+
+    app.useGlobalPipes(new ValidationPipe())
+    app.enableCors(corsOptions)
+
     await app.listen(3000)
 }
+
 bootstrap()
