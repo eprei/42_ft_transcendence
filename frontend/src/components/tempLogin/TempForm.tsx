53ce6ae3e1710d1d8011d16387a8eba788d1b70f
Robin Burri <rburri@c1r16s5.42lausanne.ch>
2023-06-08 12:49:06 +0200

    completed form frontend

--- frontend/src/components/tempLogin/TempForm.tsx
+++ frontend/src/components/tempLogin/TempForm.tsx
@@ -1,13 +1,88 @@
-const TempForm = () => {
-    return (
-        <div >
-            <form autoComplete="off">
-                <div></div>
-                <div>
-                    <p>Form has been submitted successfully</p>
+import * as React from 'react'
+import { useState } from 'react'
 
-                    <button>Submit Form</button>
+import styles from './TempForm.module.css'
+
+const TempForm = () => {
+    const [enteredName, setEnteredName] = useState('')
+    const [enteredEmail, setEnteredEmail] = useState('')
+    const [enteredPicUrl, setEnteredPicUrl] = useState('')
+
+    const nameChangeHandler = (e: React.FormEvent<HTMLInputElement>): void => {
+        setEnteredName(e.currentTarget.value)
+    }
+
+    const emailChangeHandler = (e: React.FormEvent<HTMLInputElement>): void => {
+        setEnteredEmail(e.currentTarget.value)
+    }
+
+    const urlChangeHandler = (e: React.FormEvent<HTMLInputElement>): void => {
+        setEnteredPicUrl(e.currentTarget.value)
+    }
+
+    const submitFormHandler = (
+        event: React.FormEvent<HTMLFormElement>
+    ): void => {
+        event.preventDefault()
+        if (
+            enteredEmail.trim().length > 0 &&
+            enteredName.trim().length > 0 &&
+            enteredPicUrl.trim().length > 0
+        ) {
+            const user = {
+                name: enteredName,
+                email: enteredEmail,
+                picUrl: enteredPicUrl,
+            }
+            console.log(user)
+        }
+
+        setEnteredName('')
+        setEnteredEmail('')
+        setEnteredPicUrl('')
+    }
+
+    return (
+        <div className={styles.container}>
+            <form
+                autoComplete="off"
+                className={styles.form}
+                onSubmit={submitFormHandler}
+            >
+                <h2>Sign In Now</h2>
+                <div className={styles.inputLabelPair}>
+                    <label htmlFor="login">42 Login name:</label>
+                    <input
+                        type="text"
+                        name="login"
+                        id="login"
+                        onChange={nameChangeHandler}
+                        value={enteredName}
+                    />
                 </div>
+                <div className={styles.inputLabelPair}>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        onChange={emailChangeHandler}
+                        value={enteredEmail}
+                    />
+                </div>
+                <div className={styles.inputLabelPair}>
+                    <label htmlFor="pic-url">Pic url:</label>
+                    <input
+                        type="text"
+                        name="pic-url"
+                        id="pic-url"
+                        onChange={urlChangeHandler}
+                        value={enteredPicUrl}
+                    />
+                </div>
+                <button type="submit" className={styles.btn}>
+                    Sign In
+                </button>
             </form>
         </div>
     )
