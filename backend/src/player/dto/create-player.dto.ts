78d9f4613f8edf6e82a8ec417fe6000e6122195b
Robin Burri <rburri@c1r16s5.42lausanne.ch>
2023-06-08 13:47:59 +0200

    created: CreatePlayerDto

--- backend/src/player/dto/create-player.dto.ts
+++ backend/src/player/dto/create-player.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePlayerDto {}
+import { IsEmail, IsNotEmpty } from 'class-validator'
+
+export class CreatePlayerDto {
+    id: number
+
+    @IsNotEmpty()
+    login: string
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string
+
+    avatarUrl: string
+
+    nbVictory: number
+
+    totalPlay: number
+
+    xp: number
+}
