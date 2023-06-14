062f2a0ee81fcb08c5205e35be198e27120ccaac
mpons <mpons@student.42lausanne.ch>
2023-06-14 20:37:02 +0200

    CreateChannel button effect

--- frontend/src/components/chat/ChannelList.tsx
+++ frontend/src/components/chat/ChannelList.tsx
@@ -1,4 +1,6 @@
 import styles from './ChannelList.module.css'
+import IconAddChannel from '../../assets/icon/add_friend.svg'
+// import ClickableIcon from '../profile/ClickableIcon'
 
 function ChannelList() {
     return (
@@ -7,10 +9,16 @@ function ChannelList() {
                 <h2> channel list </h2>
 
                 <ul>
-                    <li className={`${styles.chList} ${styles.newCh}`}>
-                        Create new channel +
+                    <li>
+						<button  className={`${styles.chList} ${styles.newCh}`}>
+					 	   Create new channel 
+						<img
+                            src={IconAddChannel}
+                            alt="plus sign"
+                            className={styles.addChannelIcon}
+                        />
+						</button>
                     </li>
-                    <li className={`${styles.chList}`}>Create new channel</li>
                     <li className={`${styles.chList} ${styles.silent}`}>
                         #PublicChannel1
                     </li>
