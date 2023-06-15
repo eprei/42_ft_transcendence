418f5d837c9147e04a963a6405214a53c541bd35
Robin Burri <robinburri@tutanota.com>
2023-06-15 15:53:54 +0200

    Temporary page to create users in db (#27)
    - new page in the front : /tmp
    You can create a new Player in the database, get existing players and delete them.
    
    This feature is TEMPORARY and will be deleted later when it won't be needed anymore.

---
+++ backend/src/player/player.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Player } from 'src/typeorm/Player'
+import { CreatePlayerDto } from './dto/create-player.dto'
+import { Repository } from 'typeorm'
+
+@Injectable()
+export class PlayerService {
+    constructor(
+        @InjectRepository(Player)
+        private readonly playerRepository: Repository<Player>
+    ) {}
+    create(createPlayerDto: CreatePlayerDto) {
+        const newPlayer = this.playerRepository.create(createPlayerDto)
+        return this.playerRepository.save(newPlayer)
+    }
+
+    findAll() {
+        return this.playerRepository.find()
+    }
+
+    findOne(id: number) {
+        return `This action returns a #${id} player`
+    }
+
+    async remove(login: string) {
+        const player = await this.playerRepository.delete({ login: login })
+        return player
+    }
+}
---
+++ frontend/src/components/tempLogin/TempForm.tsx
@@ -0,0 +1,98 @@
+import * as React from 'react'
+import { useState } from 'react'
+import { Player } from '../../types/Player'
+import styles from './TempForm.module.css'
+
+interface TempFormProps {
+    submitNewPlayer: (user: Player) => void
+    getUsersHandler: () => void
+}
+
+const TempForm = (props: TempFormProps) => {
+    const [enteredName, setEnteredName] = useState('')
+    const [enteredEmail, setEnteredEmail] = useState('')
+    const [enteredPicUrl, setEnteredPicUrl] = useState('')
+
+    const nameChangeHandler = (e: React.FormEvent<HTMLInputElement>): void => {
+        setEnteredName(e.currentTarget.value)
+    }
+
+    const emailChangeHandler = (e: React.FormEvent<HTMLInputElement>): void => {
+        setEnteredEmail(e.currentTarget.value)
+    }
+
+    const urlChangeHandler = (e: React.FormEvent<HTMLInputElement>): void => {
+        setEnteredPicUrl(e.currentTarget.value)
+    }
+
+    const submitFormHandler = (
+        event: React.FormEvent<HTMLFormElement>
+    ): void => {
+        event.preventDefault()
+        if (
+            enteredEmail.trim().length > 0 &&
+            enteredName.trim().length > 0 &&
+            enteredPicUrl.trim().length > 0
+        ) {
+            const user = {
+                login: enteredName,
+                email: enteredEmail,
+                avatarUrl: enteredPicUrl,
+            }
+            props.submitNewPlayer(user)
+        }
+        setEnteredName('')
+        setEnteredEmail('')
+        setEnteredPicUrl('')
+    }
+
+    return (
+        <div className={styles.container}>
+            <form
+                autoComplete="off"
+                className={styles.form}
+                onSubmit={submitFormHandler}
+            >
+                <h2>Sign In Now</h2>
+                <div className={styles.inputLabelPair}>
+                    <label htmlFor="login">42 Login name:</label>
+                    <input
+                        type="text"
+                        name="login"
+                        id="login"
+                        onChange={nameChangeHandler}
+                        value={enteredName}
+                    />
+                </div>
+                <div className={styles.inputLabelPair}>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        onChange={emailChangeHandler}
+                        value={enteredEmail}
+                    />
+                </div>
+                <div className={styles.inputLabelPair}>
+                    <label htmlFor="pic-url">Pic url:</label>
+                    <input
+                        type="text"
+                        name="pic-url"
+                        id="pic-url"
+                        onChange={urlChangeHandler}
+                        value={enteredPicUrl}
+                    />
+                </div>
+                <button type="submit" className={styles.btn}>
+                    Add User
+                </button>
+                <button className={styles.btn} onClick={props.getUsersHandler}>
+                    Get User
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default TempForm
