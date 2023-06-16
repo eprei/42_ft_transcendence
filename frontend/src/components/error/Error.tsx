768b999be04da8fe4ca6709333a1cdddbf3a6fa0
Robin Burri <robin@Robins-MacBook-Air.local>
2023-06-16 08:55:54 +0200

    add Navbar in ErrorPage

--- frontend/src/components/error/Error.tsx
+++ frontend/src/components/error/Error.tsx
@@ -1,13 +1,16 @@
+import Navbar from '../navigation/Navbar'
 import styles from './Error.module.css'
 import ErrorDisplay from './ErrorDisplay'
 
 const ErrorPage = () => {
-    const title = '404'
+    const   title = '404'
     const text = "This page doesn't exists."
-    return (
+    return (<>
+        <Navbar></Navbar>
         <div className={styles.container}>
             <ErrorDisplay title={title} text={text}></ErrorDisplay>
         </div>
+        </>
     )
 }
 
