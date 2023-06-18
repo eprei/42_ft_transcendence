9c2c9855d409c9d675ba1b4e2c1d9efd136011f8
Shaolin <shaolin@at.home>
2023-06-18 21:10:22 +0200

    button works

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -4,9 +4,11 @@ const SignIn = () => {
     return (
         <div className="signIn">
             <a
-                href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-148ab253d8e4595378ee6c5fc7bbb8c318c3359652d6b6508c71f2abf0bfceaf&redirect_uri=http%3A%2F%2Flocalhost%2Ftmp&response_type=code"
-                className={styles.container}
-            >Sign-in with 42</a>
+                href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-148ab253d8e4595378ee6c5fc7bbb8c318c3359652d6b6508c71f2abf0bfceaf&redirect_uri=http%3A%2F%2Flocalhost%3A4040%2Ftmp&response_type=code"
+                className={styles['oauth-button']}
+            >
+                Sign-in with 42
+            </a>
         </div>
     )
 }
