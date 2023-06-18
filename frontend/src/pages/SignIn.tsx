5cf3c60b7a798b2899bd0114ad2f1fdbd51f4541
Shaolin <shaolin@at.home>
2023-06-18 18:28:14 +0200

    button with link, css not working yet

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,7 +1,14 @@
-import MainSection from '../components/signin/MainSection'
+import styles from './SignIn.module.css'
 
-const Product = () => {
-    return <MainSection></MainSection>
+const SignIn = () => {
+    return (
+        <div className="signIn">
+            <a
+                href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-148ab253d8e4595378ee6c5fc7bbb8c318c3359652d6b6508c71f2abf0bfceaf&redirect_uri=http%3A%2F%2Flocalhost%2Ftmp&response_type=code"
+                className={styles.container}
+            >Sign-in with 42</a>
+        </div>
+    )
 }
 
-export default Product
+export default SignIn
