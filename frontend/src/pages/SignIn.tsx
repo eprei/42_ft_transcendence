9594e18b0dc31476dce5962349fb42f550264634
Shaolin <shaolin@at.home>
2023-06-19 22:50:46 +0200

    signin button bigger and centered

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -2,13 +2,10 @@ import styles from './SignIn.module.css'
 
 const SignIn = () => {
     return (
-        <div className="signIn">
-            <a
-                href="http://localhost:8080/api/auth/42"
-                className={styles['oauth-button']}
-            >
-                Sign-in with 42
-            </a>
+	<div className={styles['center']}>
+            	<a href="http://localhost:8080/api/auth/42" className={styles['oauth-button']}>
+                	Sign-in with 42
+		</a>
         </div>
     )
 }
