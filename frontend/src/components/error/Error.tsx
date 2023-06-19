e8315b9846bc14b6f308e5a4d82acb8f90f4f395
Robin Burri <robin@Robins-MacBook-Air.local>
2023-06-19 22:29:57 +0200

    ErrorPage back to previous state

--- frontend/src/components/error/Error.tsx
+++ frontend/src/components/error/Error.tsx
@@ -1,21 +1,11 @@
-import {
-    isRouteErrorResponse,
-    useRouteError,
-} from '../../../node_modules/react-router-dom/dist/index'
 import Navbar from '../navigation/Navbar'
 import styles from './Error.module.css'
 import ErrorDisplay from './ErrorDisplay'
 
 const ErrorPage = () => {
-    const error = useRouteError()
     let title = '404'
     let text = "This page doesn't exists."
-    if (isRouteErrorResponse(error)) {
-        if (error.status === 401) {
-            title = error.status.toString()
-            text = error.data.message
-        }
-    }
+
     return (
         <>
             <Navbar></Navbar>
