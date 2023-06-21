63cce90587945e350b03d30f73f1647447b13539
Robin Burri <robin@Robins-MacBook-Air.local>
2023-06-21 14:02:54 +0200

    fix typo

--- frontend/src/components/error/Error.tsx
+++ frontend/src/components/error/Error.tsx
@@ -4,7 +4,7 @@ import ErrorDisplay from './ErrorDisplay'
 
 const ErrorPage = () => {
     let title = '404'
-    let text = "This page doesn't exists."
+    let text = "This page doesn't exist."
     return (
         <>
             <Navbar></Navbar>
