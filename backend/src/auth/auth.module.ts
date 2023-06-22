a6c0cdc95ea033236f2826d19c105b6cf68e0bcc
Shaolin <shaolin@at.home>
2023-06-23 00:51:21 +0200

    cors session & passport for cookies

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { Auth42Strategy } from './auth42.strategy'
 import { AuthService } from './auth.service';
+import { SessionSerializer } from './session.serializer';
 
 
 @Module({
+  imports: [
+    PassportModule.register({ session: true }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, Auth42Strategy]
+  providers: [AuthService, Auth42Strategy, SessionSerializer]
 })
 export class AuthModule {}
 
