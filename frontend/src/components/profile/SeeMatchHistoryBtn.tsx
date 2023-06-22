3aa596b6acbc255772439afa0445079e972c4a16
eprei <93380002+eprei@users.noreply.github.com>
2023-06-22 22:22:58 +0200

    Update frontend/src/components/profile/SeeMatchHistoryBtn.tsx
    We name this as the filename
    
    Co-authored-by: |°_°| <grivel.theo@protonmail.com>

--- frontend/src/components/profile/SeeMatchHistoryBtn.tsx
+++ frontend/src/components/profile/SeeMatchHistoryBtn.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import styles from './SeeMatchHistoryBtn.module.css'
 
-const SeeMatchHistory = () => {
+const SeeMatchHistoryBtn = () => {
     return (
         <Link to="/history" className={styles.btn}>
             see match history
