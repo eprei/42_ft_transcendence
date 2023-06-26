8d04a2e77a77f7c261262ec48425ddcb5646f63a
shaolin-peanut <shaolinpeanut@gmail.com>
2023-06-26 16:02:10 +0200

    cleanup & delete

--- backend/src/auth/auth.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AuthService {
-    // async validateUser(profile: any): Promise<any> {
-    //     let user = { id: profile.id, username: profile.username };
-    //     return user;
-    // }
-}
