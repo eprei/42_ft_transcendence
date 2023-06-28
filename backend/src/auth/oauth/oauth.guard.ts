7890252dd1ca5fa19e3de10a204f5f161cad9fdf
shaolin-peanut <shaolinpeanut@gmail.com>
2023-06-28 15:59:48 +0200

    rename authenticatedGuard to OauthGuard

--- backend/src/auth/oauth/oauth.guard.ts
+++ backend/src/auth/oauth/oauth.guard.ts
@@ -2,7 +2,7 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
-export class AuthenticatedGuard extends AuthGuard('oauth') {
+export class OauthGuard extends AuthGuard('oauth') {
     async canActivate(context: ExecutionContext) {
         const result = (await super.canActivate(context)) as boolean
         const request = context.switchToHttp().getRequest()
