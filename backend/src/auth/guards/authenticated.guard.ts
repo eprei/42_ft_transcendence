511c4ae9e082b42b24df41d4b2f27a5c4994bea3
eprei <93380002+eprei@users.noreply.github.com>
2023-07-04 22:04:29 +0200

    Authenticated guard creation
    - AuthenticatedGuard is a single guard
    that will be in charge of protecting
    endpoints by verifying that all necessary
    authentications have been successfully
    performed (42login + 2fa if the user has
    it enabled).
    
    - Addition of explanatory comments

--- backend/src/auth/guards/authenticated.guard.ts
+++ backend/src/auth/guards/authenticated.guard.ts
@@ -5,6 +5,30 @@ export class AuthenticatedGuard implements CanActivate {
     async canActivate(context: ExecutionContext) {
         const request = context.switchToHttp().getRequest()
 
-        return request.isAuthenticated()
+        return request.isAuthenticated() // && !request.session.totpRequire
+		/// TODO Here we will verify that the 2fa authentication has been
+		// performed in case the user has enabled this option. This way we
+		// will have a unique guard that will verify that the two
+		// authentication factors have been correctly validated.
     }
 }
+
+/*
+
+This guard checks if a user is authenticated before allowing access to
+a path or endpoint. It relies on the isAuthenticated() method provided by
+Passport and Express sessions to perform the verification.
+
+In the canActivate method, the execution context is obtained by
+context.switchToHttp().getRequest(). This provides access to the current HTTP
+request.
+
+It then checks if the request has an isAuthenticated() method. This method is
+provided by the Passport integration and Express sessions, and returns true if
+the user is authenticated or false if not.
+
+The result of the check is returned as the result of the canActivate method. If
+the user is authenticated, access to the protected path is allowed. If the user is
+not authenticated, access is blocked and the request is denied.
+
+*/
