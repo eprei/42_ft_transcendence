c23f1075ec45c5ff35d115e11e841014e34b8765
mpons <mpons@student.42lausanne.ch>
2023-07-07 21:20:56 +0200

    Makefile format in two lines

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -50,6 +50,15 @@ async function bootstrap() {
         next()
     })
 
+	const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
     await app.listen(3000)
 }
 
