bcb11e445b5951929dbf813d4c0cc717616c885b
Robin Burri <rburri@c1r16s5.42lausanne.ch>
2023-07-10 08:23:50 +0200

    add validateUsera in auth.service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common'
+import { UserService } from '../user/user.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UserService) {}
+
+    async validateUser(FT_id: string, name: string, avatarUrl: string) {
+        const existing_user = await this.userService.findByFT_id(FT_id);
+        if (existing_user) {
+            return existing_user;
+        } else
+            return await this.userService.create({ 
+                FT_id, 
+                nickname: name, 
+                avatarUrl
+            });
+    }
+}
