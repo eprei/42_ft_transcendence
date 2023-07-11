c7f82db645309950ced1e85af886a5380c2f696a
Robin Burri <robinburri@tutanota.com>
2023-07-11 22:34:40 +0200

    R real user in front (#72)
    When you sign in with 42, a user is created in the database using the data provided by 42.
    The user is stored in the redux store.

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common'
+import { UserService } from '../user/user.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UserService) {}
+
+    async validateUser(user42) {
+        let user = await this.userService.findByFT_id(user42.FT_id)
+        if (!user) {
+            user = await this.userService.create({
+                FT_id: user42.FT_id,
+                nickname: user42.nickname,
+                avatarUrl: user42.avatarUrl,
+            })
+        }
+        return user
+    }
+}
