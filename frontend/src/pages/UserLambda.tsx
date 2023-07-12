af42d157b03de1ba29be5387583997a879704ee5
eprei <93380002+eprei@users.noreply.github.com>
2023-07-12 22:34:02 +0200

    Incorrect URL management
    In case of a request to view the page of a
    user that does not exist, the user is
    redirected to the profile page

--- frontend/src/pages/UserLambda.tsx
+++ frontend/src/pages/UserLambda.tsx
@@ -1,67 +1,68 @@
-import { useEffect, useState } from 'react'
-import styles from './MainProfile.module.css'
-import Statistics from '../components/profile/Statistics'
-import { useParams } from 'react-router-dom'
-import UserLambdaInformation from '../components/UserLambda/UserLambdaInformation'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styles from './MainProfile.module.css';
+import Statistics from '../components/profile/Statistics';
+import UserLambdaInformation from '../components/UserLambda/UserLambdaInformation';
 
 export interface UserData {
-    user: {
-        nickname: string
-        avatarUrl: string
-        nbVictory: number
-        totalPlay: number
-        xp: number
-        TFAEnabled: boolean
-    }
-    userPosition: number
+  user: {
+    nickname: string;
+    avatarUrl: string;
+    nbVictory: number;
+    totalPlay: number;
+    xp: number;
+    TFAEnabled: boolean;
+  };
+  userPosition: number;
 }
 
 export interface UserInformationProps {
-    userData: UserData
+  userData: UserData;
 }
 
 const UserLambda = () => {
-    const [loading, setLoading] = useState(true)
-    const [userData, setUserData] = useState<UserData>({} as UserData)
-    const { nickname } = useParams<{ nickname: string }>()
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState<UserData>({} as UserData);
+  const { nickname } = useParams<{ nickname: string }>();
 
-    useEffect(() => {
-        getCurrentUser()
-    }, [])
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
 
-    async function getCurrentUser() {
-        try {
-            const response = await fetch(
-                `http://localhost:8080/api/user/nickname/${nickname}`,
-                {
-                    method: 'GET',
-                    credentials: 'include',
-                }
-            )
-            if (!response.ok) {
-                throw new Error('Failed to fetch user')
-            }
-            const data = await response.json()
-            setUserData({ user: data, userPosition: data.userPosition })
-            setLoading(false)
-        } catch (error) {
-            console.log('Error:', error)
+  async function getCurrentUser() {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/user/nickname/${nickname}`,
+        {
+          method: 'GET',
+          credentials: 'include',
         }
+      );
+      if (!response.ok) {
+        throw new Error('Failed to fetch user');
+      }
+      const data = await response.json();
+      setUserData({ user: data, userPosition: data.userPosition });
+      setLoading(false);
+    } catch (error) {
+      console.log('Error:', error);
+      window.location.href = '/profile'
     }
+  }
 
-    if (loading) {
-        return <div>Loading...</div>
-    }
-    return (
-        <div className={styles.container}>
-            <div className={styles.body}>
-                <div className={styles.bodyLeftSide}>
-                    <UserLambdaInformation userData={userData} />
-                    <Statistics userData={userData} />
-                </div>
-            </div>
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className={styles.container}>
+      <div className={styles.body}>
+        <div className={styles.bodyLeftSide}>
+          <UserLambdaInformation userData={userData} />
+          <Statistics userData={userData} />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default UserLambda
+export default UserLambda;
