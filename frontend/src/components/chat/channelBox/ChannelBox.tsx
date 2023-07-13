abac381ebdb76458290a1e401cd003becf5ea814
Robin Burri <rburri@c1r16s5.42lausanne.ch>
2023-07-13 11:09:19 +0200

    wip

--- frontend/src/components/chat/channelBox/ChannelBox.tsx
+++ frontend/src/components/chat/channelBox/ChannelBox.tsx
@@ -11,6 +11,26 @@ import { joinedChannelAtom } from './ChannelLi'
 
 const socket = io('http://localhost:8080')
 
+async function createNewChannel(data: CreateChannel) {
+    try {
+        const response = await fetch('http://localhost:8080/api/channel', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to make POST request')
+        }
+
+        const responseData = await response.json()
+        return responseData
+    } catch (error) {
+        console.error(error)
+    }
+}
 const ChannelBox = () => {
     const [allChan, setAllChan] = useState<Channel[]>([])
     const [joinedChannel] = useAtom(joinedChannelAtom)
