0897a63683d5071b5642d20b48fc322a3bbfe4ce
mpons <mpons@student.42lausanne.ch>
2023-07-16 22:44:15 +0200

    autoscroll OK

--- frontend/src/components/chat/chatBox/ChatFeed.tsx
+++ frontend/src/components/chat/chatBox/ChatFeed.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useAtom } from 'jotai'
 import Msg from './Msg'
 import styles from './ChatFeed.module.css'
 import { chatIdAtom } from '../channelBox/ChannelLi'
-// import { io } from 'socket.io-client'
-
-// const socket = io('http://localhost:8080')
 
 interface ChatFeedProps {
     socket: any
@@ -13,26 +10,28 @@ interface ChatFeedProps {
 
 const ChatFeed = ({ socket }: ChatFeedProps ) => {
 
-
+	const isFeedFull = useRef<HTMLDivElement>(null)
 	const [chatId] = useAtom(chatIdAtom)
 
-	socket.on('incomingMessage', (newMessage) => {
+	socket.on('incomingMessage', (newMessage: any) => {
 		const msgCpy = [...msgs]
 		msgCpy.push(newMessage)
 		setMsgs(msgCpy);
-		// setMsgs((msgs) => [...msgs, newMessage])
-
 	})
     
     const [msgs, setMsgs] = useState<any[]>([])
 
     useEffect(() => {
         if (chatId)
-			// getChMsgHandler()
 			getAllMsgSocket()
         else
 			setMsgs([])
     }, [chatId])
+
+	useEffect(() => {
+		isFeedFull.current?.scrollIntoView()
+	}, [msgs])
+
 
 	const getAllMsgSocket = () => {
 		socket.emit('findAllMsgByChannel', chatId, (response: any) => {
@@ -46,6 +45,7 @@ const ChatFeed = ({ socket }: ChatFeedProps ) => {
             {msgs.map((msg) => (
                 <Msg key={msg.id} msg={msg}></Msg>
             ))}
+		 <div ref={isFeedFull}></div>
         </div>
     )
 }
