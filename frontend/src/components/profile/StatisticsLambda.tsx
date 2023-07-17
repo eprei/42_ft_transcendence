b5b3518763b6d22dbd2ce602bdd0a942c8c71ea0
eprei <93380002+eprei@users.noreply.github.com>
2023-07-17 09:27:35 +0200

    UserLambda page bugfix (#77)
    The functionality did not work because the
    element was based on an old version of the
    'statistics' element.
    
    Hotfix: recreate the old element and call it
    StatisticsLambda.

---
+++ frontend/src/components/profile/StatisticsLambda.tsx
@@ -0,0 +1,37 @@
+import styles from './Statistics.module.css'
+import StatisticElement from './StatisticElement'
+import IconVictoire from '../../assets/icon/victoires.svg'
+import IconDefeat from '../../assets/icon/defeats.svg'
+import IconRanking from '../../assets/icon/ranking.svg'
+import { UserInformationProps } from '../../pages/MainProfile'
+
+const StatisticsLambda = ({ userData }: UserInformationProps) => {
+    return (
+        <div>
+            <div className={styles.container}>
+                <div className={styles.subtitle}>Statistics</div>
+                <div className={styles.horizontalList}>
+                    <StatisticElement
+                        icon={IconVictoire}
+                        text="Victorires"
+                        number={userData.user.nbVictory}
+                    />
+                    <StatisticElement
+                        icon={IconDefeat}
+                        text="Defeats"
+                        number={
+                            userData.user.totalPlay - userData.user.nbVictory
+                        }
+                    />
+                    <StatisticElement
+                        icon={IconRanking}
+                        text="Ranking"
+                        number={userData.user.userPosition}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StatisticsLambda
