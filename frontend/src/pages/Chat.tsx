45772eb1e68dc6a825b7495e08870efadfd814b9
Robin Burri <robin@Robins-MacBook-Air.local>
2023-07-21 08:30:04 +0200

    fetch user.id in Chat loader

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -2,8 +2,15 @@ import styles from './Chat.module.css'
 import ChannelBox from '../components/chat/channelBox/ChannelBox.tsx'
 import ChatBox from '../components/chat/chatBox/ChatBox'
 import UserList from '../components/chat/userBox/UserBox.tsx'
+import { userActions } from '../store/user'
+import { useAppDispatch } from '../store/types'
+import { UserData } from '../types/UserData'
+import { useLoaderData } from 'react-router-dom'
 
 const Chat = () => {
+    const fetchUserData = useLoaderData() as UserData
+    const dispatch = useAppDispatch()
+    dispatch(userActions.update({ user: fetchUserData }))
     return (
         <div className={styles.chatContainer}>
             <ChannelBox />
@@ -14,3 +21,19 @@ const Chat = () => {
 }
 
 export default Chat
+
+export async function loader() {
+    const response = await fetch(`http://localhost:8080/api/user/me`, {
+        method: 'GET',
+        credentials: 'include',
+    })
+
+    if (response.status !== 200) {
+        throw new Response(
+            JSON.stringify({ message: 'Error fetching user data' })
+        )
+    }
+
+    const data = await response.json()
+    return data
+}
