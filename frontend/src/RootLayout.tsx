072001e54ea0b1e2234c1a5270a257ed2dd5d7d8
Robin Burri <robin@Robins-MacBook-Air.local>
2023-07-21 08:20:40 +0200

    changed to only fetch user status

--- frontend/src/RootLayout.tsx
+++ frontend/src/RootLayout.tsx
@@ -1,36 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/navigation/Navbar'
 import { authActions } from './store/auth'
-import { userActions } from './store/user'
 import { useAppDispatch } from './store/types'
-import { UserData } from './types/UserData'
 import { useLoaderData } from 'react-router-dom'
 
 interface dataStatus {
     status: string
 }
 
-async function getUserData() {
-    const response = await fetch(`http://localhost:8080/api/user/me`, {
-        method: 'GET',
-        credentials: 'include',
-    })
-
-    if (response.status !== 200) {
-        throw new Response(
-            JSON.stringify({ message: 'Error fetching user data' })
-        )
-    }
-
-    const data = (await response.json()) as UserData
-    return data
-}
-
 const RootLayout = () => {
     async function checkStatus(data: dataStatus) {
         if (data.status === 'isLogged') {
-            const user = await getUserData()
-            dispatch(userActions.update({ user: user }))
             dispatch(authActions.login())
         } else if (data.status === 'need2fa') {
             dispatch(authActions.setNeed2fa())
@@ -40,7 +20,6 @@ const RootLayout = () => {
     }
     const dispatch = useAppDispatch()
     const data = useLoaderData() as dataStatus
-    console.log('data received: ', data)
     checkStatus(data)
 
     return (
