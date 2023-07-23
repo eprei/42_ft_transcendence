6f16d0d966af5c9f349fdd8730c20c548a81f228
Theo Grivel <grivel.theo@protonmail.com>
2023-07-23 13:52:46 +0200

    move variable outside of the function

--- backend/src/pong/pong.service.ts
+++ backend/src/pong/pong.service.ts
@@ -3,45 +3,46 @@ import { CreatePongDto } from './dto/create-pong.dto'
 import { UpdatePongDto } from './dto/update-pong.dto'
 import { Frame } from './entities/pong.entity'
 
+const PADDLE_WIDTH: number = 10
+const PADDLE_HEIGHT: number = 50
+const BALL_SIZE: number = 10
+
 @Injectable()
 export class PongService {
+    frame: Frame = {
+        paddleLeft: {
+            position: {
+                x: 10,
+                y: 20,
+            },
+            size: {
+                width: PADDLE_WIDTH,
+                height: PADDLE_HEIGHT,
+            },
+        },
+        paddleRight: {
+            position: {
+                x: 280,
+                y: 20,
+            },
+            size: {
+                width: PADDLE_WIDTH,
+                height: PADDLE_HEIGHT,
+            },
+        },
+        ball: {
+            position: {
+                x: 50,
+                y: 50,
+            },
+            size: {
+                width: BALL_SIZE,
+                height: BALL_SIZE,
+            },
+        },
+    }
+
     simpleFrame(): Frame {
-        const PADDLE_WIDTH: number = 10
-        const PADDLE_HEIGHT: number = 50
-        const BALL_SIZE: number = 10
-        console.log('coucou')
-        let frame: Frame = {
-            paddleLeft: {
-                position: {
-                    x: 10,
-                    y: 20,
-                },
-                size: {
-                    width: PADDLE_WIDTH,
-                    height: PADDLE_HEIGHT,
-                },
-            },
-            paddleRight: {
-                position: {
-                    x: 280,
-                    y: 20,
-                },
-                size: {
-                    width: PADDLE_WIDTH,
-                    height: PADDLE_HEIGHT,
-                },
-            },
-            ball: {
-                position: {
-                    x: 50,
-                    y: 50,
-                },
-                size: {
-                    width: BALL_SIZE,
-                    height: BALL_SIZE,
-                },
-            },
-        }
-        return frame
+        return this.frame
     }
 }
