04151afd272218ac21c32a36aa9ad8f8676860fd
Robin Burri <robin@Robins-MacBook-Air.local>
2023-07-25 21:09:19 +0200

    chatFeed is simplified the logic is in ChatBox

--- frontend/src/components/chat/chatBox/ChatBox.tsx
+++ frontend/src/components/chat/chatBox/ChatBox.tsx
@@ -3,17 +3,42 @@ import ChatFeed from './ChatFeed.tsx'
 import SendForm from './SendForm.tsx'
 import { io } from 'socket.io-client'
 import { useAppSelector } from '../../../store/types'
+import { useEffect, useState } from 'react'
 
-const socket = io('http://localhost:8080')
+
 
 function ChatBox() {
+    const socket = io('http://localhost:8080')
     const currentChatSelected = useAppSelector(
         (state) => state.chat.currentChatSelected
     ) as number
+    const [messages, setMesssages] = useState<any[]>([])
+    const getAllMsgSocket = () => {
+        socket.emit(
+            'findAllMsgByChannel',
+            currentChatSelected,
+            (response: any) => {
+                console.log(response)
+                setMesssages(response)
+            }
+        )
+    }
+
+    socket.on('incomingMessage', (newMessage: any) => {
+        const msgCpy = [...messages]
+        msgCpy.push(newMessage)
+        setMesssages(msgCpy)
+    })
+
+    useEffect(() => {
+        if (currentChatSelected) getAllMsgSocket()
+        else setMesssages([])
+    }, [currentChatSelected])
+
 
     return (
         <div className={styles.chatBox}>
-            <ChatFeed socket={socket} />
+            <ChatFeed messages={messages} />
             {currentChatSelected && <SendForm socket={socket} />}
         </div>
     )
--- frontend/src/components/chat/chatBox/ChatFeed.tsx
+++ frontend/src/components/chat/chatBox/ChatFeed.tsx
@@ -1,52 +1,24 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import Msg from './Msg'
 import styles from './ChatFeed.module.css'
-import { useAppSelector } from '../../../store/types'
 
 interface ChatFeedProps {
-    socket: any
+    messages: any[]
 }
 
-const ChatFeed = ({ socket }: ChatFeedProps) => {
+const ChatFeed = ({ messages }: ChatFeedProps) => {
     const isFeedFull = useRef<HTMLDivElement>(null)
-    const currentChatSelected = useAppSelector(
-        (state) => state.chat.currentChatSelected
-    ) as number
-
-    socket.on('incomingMessage', (newMessage: any) => {
-        const msgCpy = [...msgs]
-        msgCpy.push(newMessage)
-        setMsgs(msgCpy)
-    })
-
-    const [msgs, setMsgs] = useState<any[]>([])
 
     useEffect(() => {
-        if (currentChatSelected) getAllMsgSocket()
-        else setMsgs([])
-    }, [currentChatSelected])
-
-    useEffect(() => {
-        isFeedFull.current?.scrollIntoView()
-    }, [msgs])
-
-    const getAllMsgSocket = () => {
-        socket.emit(
-            'findAllMsgByChannel',
-            currentChatSelected,
-            (response: any) => {
-                console.log(response)
-                setMsgs(response)
-            }
-        )
-    }
+        if (isFeedFull.current)
+            isFeedFull.current.scrollTop = isFeedFull.current?.scrollHeight
+    }, [messages])
 
     return (
-        <div className={styles.container}>
-            {msgs.map((msg) => (
+        <div className={styles.container} ref={isFeedFull}>
+            {messages.map((msg) => (
                 <Msg key={msg.id} msg={msg}></Msg>
             ))}
-            <div ref={isFeedFull}></div>
         </div>
     )
 }
