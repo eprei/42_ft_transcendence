1edf719a0cbaeca35ff312aa198a410c42393361
|°_°| <grivel.theo@protonmail.com>
2023-07-26 11:31:19 +0200

    backend: create Frame entity

--- backend/src/pong/entities/pong.entity.ts
+++ backend/src/pong/entities/pong.entity.ts
@@ -1,1 +1,20 @@
-export class Pong {}
+interface Position {
+    x: number
+    y: number
+}
+
+interface Size {
+    width: number
+    height: number
+}
+
+interface Rectangle {
+    position: Position
+    size: Size
+}
+
+export interface Frame {
+    paddleLeft: Rectangle
+    paddleRight: Rectangle
+    ball: Rectangle
+}
