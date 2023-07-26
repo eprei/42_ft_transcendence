b5d73cf9a34da3852375b5a1d9983fd2118bbb7d
Shaolin <shaolin@at.home>
2023-07-26 19:37:26 +0200

    a few fixes in css

--- frontend/src/pages/MatchHistory.tsx
+++ frontend/src/pages/MatchHistory.tsx
@@ -1,16 +1,10 @@
 import { MatchList } from '../components/history/MatchList'
+import styles from './MatchHistory.module.css'
 
 const MatchHistory = () => {
     return (
         <div>
-            <h1
-                style={{
-                    textAlign: 'center',
-                    marginBottom: '1rem',
-                }}
-            >
-                Match History
-            </h1>
+            <h1 className={styles.title}>Match History</h1>
             <MatchList />
         </div>
     )
