5159c4c42d8624213e57ee8e1c9eec725142b3af
Robin Burri <robin@Robins-MacBook-Air.local>
2023-07-29 18:37:16 +0200

    deleted useless import in app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,11 +7,9 @@ import { UserModule } from './user/user.module'
 import { ChannelModule } from './channel/channel.module'
 import { User } from './typeorm/user.entity'
 import { Channel } from './typeorm/channel.entity'
-import { MessageModule } from './message/message.module'
 import { Message } from './typeorm/message.entity'
 import { Friend } from './typeorm/friend.entity'
 import { Match } from './typeorm/match.entity'
-import { ChannelUserMuted } from './typeorm/channel-user-muted.entity'
 import { FriendModule } from './friend/friend.module'
 import { MatchModule } from './match/match.module'
 import { AuthModule } from './auth/auth.module'
@@ -20,7 +18,6 @@ import { AuthenticatedGuard } from './auth/guards/authenticated.guard'
 import { APP_GUARD } from '@nestjs/core'
 import { RoomModule } from './room/room.module'
 import { ChatModule } from './chat/chat.module'
-import { ChannelUserMutedModule } from './channel-user-muted/channel-user-muted.module'
 
 @Module({
     imports: [
@@ -30,7 +27,7 @@ import { ChannelUserMutedModule } from './channel-user-muted/channel-user-muted.
             url: process.env.DATABASE_URL,
             autoLoadEntities: true,
             synchronize: true,
-            entities: [User, Channel, Message, Friend, Match, ChannelUserMuted],
+            entities: [User, Channel, Friend, Match],
         }),
         TypeOrmModule.forFeature([
             Channel,
@@ -38,18 +35,15 @@ import { ChannelUserMutedModule } from './channel-user-muted/channel-user-muted.
             Message,
             Friend,
             Match,
-            ChannelUserMuted,
         ]),
         UserModule,
         ChannelModule,
-        MessageModule,
         FriendModule,
         MatchModule,
         AuthModule,
         PongModule,
         ChatModule,
         RoomModule,
-        ChannelUserMutedModule,
     ],
     controllers: [AppController],
     providers: [
