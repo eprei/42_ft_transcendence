b1f536dfa868863ac6742a0da8a54967c8bdf447
Robin Burri <robin@Robins-MacBook-Air.local>
2023-07-29 21:38:50 +0200

    react strict mode back on

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
@@ -5,7 +6,9 @@ import { Provider } from 'react-redux'
 import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 )
