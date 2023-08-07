15274297efb739ce5209a6bd47990fa3d3319e0f
|°_°| <grivel.theo@protonmail.com>
2023-08-07 14:19:11 +0200

    use environment variables

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -98,7 +98,7 @@ export class UserController {
             throw new BadRequestException('File is not an image')
         }
         const response = {
-            filePath: `http://localhost:8080/api/user/picture/${file.filename}`,
+            filePath: `${process.env.URL_BACKEND}/api/user/picture/${file.filename}`,
         }
 
         return response
