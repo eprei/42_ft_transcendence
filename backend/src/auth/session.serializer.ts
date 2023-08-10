b544d926ea2c4e8747011d4e84a5ea1ba31ad47d
eprei <93380002+eprei@users.noreply.github.com>
2023-08-10 14:12:19 +0200

    E avoid double login (#146)
    * The user cannot log in twice at the same time

--- backend/src/auth/session.serializer.ts
+++ backend/src/auth/session.serializer.ts
@@ -13,30 +13,3 @@ export class SessionSerializer extends PassportSerializer {
         done(null, payload)
     }
 }
-
-/*
-
-	To maintain a login session, Passport serializes and deserializes user information
-	to and from the session. The information that is stored is determined by the
-	application, which supplies a serializeUser and a deserializeUser function.
-
-	A login session is established upon a user successfully authenticating using a
-	credential. The following route will authenticate a user using a username and
-	password. If successfully verified, Passport will call the serializeUser function,
-	which in the below example is storing the user's ID, username, and picture.
-
-		passport.serializeUser(function(user, cb) {
-		process.nextTick(function() {
-			return cb(null, {
-			id: user.id,
-			username: user.username,
-			picture: user.picture
-			});
-		});
-		});
-
-	When the session is authenticated, Passport will call the deserializeUser function,
-	which in the above example is yielding the previously stored user ID, username, and
-	picture. The req.user property is then set to the yielded information.
-
-*/
