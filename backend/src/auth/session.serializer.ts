c2af8d19c69cc42197adf6d302b6d39c6e21fef7
Theo Grivel <tgrivel@c2r5s4.42lausanne.ch>
2023-08-10 14:36:57 +0200

    eprei

--- backend/src/auth/session.serializer.ts
+++ backend/src/auth/session.serializer.ts
@@ -1,15 +1,24 @@
 import { PassportSerializer } from '@nestjs/passport'
 import { Injectable } from '@nestjs/common'
+import { UserService } from 'src/user/user.service'
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
-    serializeUser(user: any, done: (err: Error, user: any) => void): any {
-        done(null, user)
+    constructor(private readonly userService: UserService) {
+        super()
     }
-    deserializeUser(
-        payload: any,
-        done: (err: Error, payload: string) => void
-    ): any {
-        done(null, payload)
+
+    serializeUser(user: any, done: (err: Error, user: any) => void): any {
+        done(null, user.id)
+    }
+    deserializeUser(payload: any, done: Function): any {
+        this.userService
+            .findOne(payload)
+            .then((user) => {
+                done(null, user)
+            })
+            .catch((err) => {
+                done(err, null)
+            })
     }
 }
